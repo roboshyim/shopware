902e49dfebc770822f9fe8a4bd09854cc256bcff
D.Neustadt <d.neustadt@shopware.com>
2026-06-30 16:03:43 +0200

    feat: generate seo urls for store api routes

--- src/Core/Content/Seo/SeoUrlRoute/EntitySeoUrlRouteInterface.php
+++ src/Core/Content/Seo/SeoUrlRoute/EntitySeoUrlRouteInterface.php
@@ -2,10 +2,14 @@
 
 namespace Shopware\Core\Content\Seo\SeoUrlRoute;
 
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\System\SalesChannel\SalesChannelEntity;
 
 #[Package('discovery')]
 interface EntitySeoUrlRouteInterface
 {
     public function getConfig(): SeoUrlRouteConfig;
+
+    public function prepareCriteria(Criteria $criteria, SalesChannelEntity $salesChannel): void;
 }
--- tests/unit/Core/Content/Seo/SeoUrlUpdaterTest.php
+++ tests/unit/Core/Content/Seo/SeoUrlUpdaterTest.php
@@ -56,17 +56,17 @@ class SeoUrlUpdaterTest extends TestCase
 
     public function testUpdateWithoutDomain(): void
     {
-        $seoUrlUpdater = $this->createSeoUrlUpdater();
+        $seoUrlUpdater = $this->createSeoUrlUpdater([], [], [new ProductPageSeoUrlRoute(new ProductDefinition())]);
 
         $this->connection->method('fetchAllAssociative')->willReturn([]);
         $this->seoUrlPersister->expects($this->never())->method('updateSeoUrls');
 
-        $seoUrlUpdater->update('test', []);
+        $seoUrlUpdater->update(ProductPageSeoUrlRoute::ROUTE_NAME, []);
     }
 
     public function testUpdateWithoutDefaultTemplates(): void
     {
-        $seoUrlUpdater = $this->createSeoUrlUpdater();
+        $seoUrlUpdater = $this->createSeoUrlUpdater([], [], [new ProductPageSeoUrlRoute(new ProductDefinition())]);
 
         $this->connection->method('fetchAllAssociative')->willReturn([
             [
@@ -79,7 +79,7 @@ class SeoUrlUpdaterTest extends TestCase
         $this->seoUrlPersister->expects($this->never())->method('updateSeoUrls');
 
         $this->expectExceptionObject(new \RuntimeException('Default templates not configured'));
-        $seoUrlUpdater->update('test', []);
+        $seoUrlUpdater->update(ProductPageSeoUrlRoute::ROUTE_NAME, []);
     }
 
     public function testUpdateWithoutRoute(): void
